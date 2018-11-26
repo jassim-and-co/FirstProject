59a6492f6b1e18ef20f4d595fa102e3dd86dd64d
shaalanjassim <jassimshaalan@gmail.com>
2018-11-26 12:27:45 +0200

    Find combination for numbe

---
+++ src/breakPointLearn.js
@@ -0,0 +1,14 @@
+function fibonacci(arg) {
+    var result_a = 1,
+        result_b = 1,
+        sum = 0;
+
+    for (var i = 0; i <= arg; i++) {
+        sum = result_a + result_b;
+        result_a = result_b;
+        result_b = sum;
+    }
+    return result_b;
+}
+console.log("Line.№  \n :"+ fibonacci(9) +"\n"+
+"---------------------------------------------")
